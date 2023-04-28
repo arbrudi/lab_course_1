3ddd1b25b046c370dc8cb489ab2e4060610ba501
Leka <lz52291@ubt-uni.net>
2023-04-28 21:46:33 +0000

    homepage demo  1

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage';
@@ -17,30 +17,31 @@ import Register from './components/Register';
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <NavBar />
-   
-    <div className="page-body">
-    <Routes>
-    
-    <Route path="/" element = {<HomePage />} />
-    <Route path="/about" element = {<AboutPage />} />
-    <Route path="/books" element = {<BookList />} />
-    <Route path="/books/:bookid" element = {<BookPage />} />
-    <Route path="/articles" element = {<ArticleList />} />
-    <Route path="/articles/:articleid" element = {<ArticlePage />} />
-    <Route path="/userdashboard" element = {<UserDashboard />} />
-    <Route path="/admindashboard" element = {<AdminDashboard />} />
-    <Route path="/*" element = {<PageNotFound />} />
-    <Route path="/login" element = {<Login/>} />
-    <Route path="/register" element = {<Register/>} />
-    
-    </Routes>
-    </div>
-    </div>
-    <div className='footer-app'>
-      <Footer />
-    </div>
+      <div className="App">
+        <NavBar />
+
+        <div className="page-body">
+          <Routes>
+
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/books" element={<BookList />} />
+            <Route path="/books/:bookid" element={<BookPage />} />
+            <Route path="/articles" element={<ArticleList />} />
+            <Route path="/articles/:articleid" element={<ArticlePage />} />
+            <Route path="/userdashboard" element={<UserDashboard />} />
+            <Route path="/admindashboard" element={<AdminDashboard />} />
+            <Route path="/*" element={<PageNotFound />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/HomePage" element={<HomePage />} />
+
+          </Routes>
+        </div>
+      </div>
+      <div className='footer-app'>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
--- Project/src/pages/HomePage.js
+++ Project/src/pages/HomePage.js
@@ -1,11 +1,78 @@
+import React from 'react';
+import './pages_css/Homepage_style.css';
+import { Carousel } from 'react-bootstrap';
 
-const HomePage = () =>{
-    return(
-        <>
-        <h1>This is the Home Page!</h1>
-
-       
-        </>
+function HomePage() {
+    return (
+        <div className="home-container">
+            <h1>Welcome to the e-Library!</h1>
+            <p>Here, you can browse through our collection of books, articles, and other resources.</p>
+            <div className="search-bar">
+                <input type="text" placeholder="Search for a book or article" />
+                <button>Search</button>
+            </div>
+            <div className="featured-books">
+                <h2>Featured Books</h2>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://via.placeholder.com/20x30"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Book Title</h3>
+                            <p>Author Name</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://via.placeholder.com/200x300"
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Book Title</h3>
+                            <p>Author Name</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://via.placeholder.com/200x300"
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Book Title</h3>
+                            <p>Author Name</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <div className="categories">
+                <h2>Browse by Category</h2>
+                <Carousel>
+                    <Carousel.Item>
+                        <div className="category-card">
+                            <img src="https://via.placeholder.com/100" alt="Category Image" />
+                            <h3>Category Name</h3>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="category-card">
+                            <img src="https://via.placeholder.com/100" alt="Category Image" />
+                            <h3>Category Name</h3>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="category-card">
+                            <img src="https://via.placeholder.com/100" alt="Category Image" />
+                            <h3>Category Name</h3>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </div>
     );
 }
 
