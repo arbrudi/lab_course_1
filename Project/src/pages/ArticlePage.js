39e394945415906759841a6cd38d3eb98df9cf57
Eris Beselica <eb51390@ubt-uni.net>
2023-06-11 10:23:37 +0200

    article page css update

--- Project/src/pages/ArticlePage.js
+++ Project/src/pages/ArticlePage.js
@@ -2,9 +2,12 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import '../pages/pages_css/Articlepage.css'
+
 import CommentArticle from "../components/CRUDS/Article_Managment/CommentArticle";
 import RatingArticle from "../components/CRUDS/Article_Managment/RatingArticle";
 import FavoriteArticle from "../components/CRUDS/Article_Managment/FavoriteArticle";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const ArticlePage = () => {
   const { Article_ID } = useParams();
@@ -12,7 +15,7 @@ const ArticlePage = () => {
 
   useEffect(() => {
     Axios.get(`http://localhost:3001/Articlelist/articles/${Article_ID}`)
-      .then(res => setArticlePage(res.data[0])) // Assuming the result is an array with a single news item
+      .then(res => setArticlePage(res.data[0]))
       .catch(err => console.log(err));
   }, [Article_ID]);
 
@@ -22,15 +25,18 @@ const ArticlePage = () => {
 
   return (
     <>
-      <h1>This is the Article PAGE!</h1>
-
-      <div>
-        <h1>{ArticlePage.Article_title}</h1>
-        <p>{ArticlePage.Article_type}</p>
-        <img src={ArticlePage.Article_image} alt={ArticlePage.Article_title} />
-        <p>{ArticlePage.Article_Description}</p>
+      <NavBar/>
+       
+      <div className="A_text"> 
+  
+        <h1 className="Title">Title:"{ArticlePage.Article_title}" </h1>
+        <h1 className="Type">Type:{ArticlePage.Article_type}</h1>
+        </div>
+       
+      <div className="A_info">
+       <img className="Img" src={ArticlePage.Article_image} alt={ArticlePage.Article_title} />
+        <p className="Description">{ArticlePage.Article_Description}</p>
       </div>
-
       <h1>Comment Section</h1>
 
 
@@ -44,7 +50,7 @@ const ArticlePage = () => {
     
  
 
-
+    <Footer/>
    </>
   );
 };
