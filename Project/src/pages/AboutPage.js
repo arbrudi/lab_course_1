d3e79831022aec2cd20f0659dd57ea2b4b4f8753
riionah <rh54044@ubt-uni.net>
2023-06-13 11:58:08 +0200

    Slidercss

--- Project/src/pages/AboutPage.js
+++ Project/src/pages/AboutPage.js
@@ -1,95 +1,93 @@
-import React, { useState, useEffect } from "react"; 
-import '../pages/pages_css/slider.css' ; 
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-import  Axios  from "axios"; 
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import "../pages/pages_css/slider.css";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import Axios from "axios";
+import { Link } from "react-router-dom";
 
-
-function AboutPage() { 
- 
+function AboutPage() {
+  const [slidercontroller, setSliderController] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
-  const slides = [
-    {
-      image: "https://d3vpszern3jgjo.cloudfront.net/wp-content/uploads/2021/01/Ebook-clear-design--992x523.jpg",
-      active: true,
-    },
-    {
-      image: "https://venngage-wordpress.s3.amazonaws.com/uploads/2019/06/eBook-BP-Header.jpg",
-      active: false,
-    },
-    {
-      image: "https://designercandies.net/wp-content/uploads/2021/02/Indesign-ebook-templates-22.jpg",
-      active: false,
-    } ,
-    {
-      image: "https://media.istockphoto.com/id/965143096/vector/set-of-brochure-design-templates-on-the-subject-of-education-school-online-learning.jpg?s=612x612&w=0&k=20&c=8wpg-LoYBkzvKN9Sng-G7O79KdAtyRhi2uRlUXaG_ho=",
-      active: false,
-    } ,
-    {
-      image: "https://i.fbcd.co/products/original/93519836ba9c2e88bfc88b6837ba15c2340e21d00ca2bf46dfa3200a9e7b2c51.jpg",
-      active: false,
-    } 
-  ];
+  const [section, setSection] = useState([]);
 
-  const[section,setSection] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/admin/text_section")
+      .then((res) => setSection(res.data))
+      .catch((err) => console.log(err));
 
-    useEffect (()=>{
-      Axios.get('http://localhost:3001/admin/text_section')
-      .then((res)=>setSection(res.data))
-      .catch((err)=>console.log(err));
-    },[]);
+    Axios.get("http://localhost:3001/admin/slidercontroller")
+      .then((res) => setSliderController(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-    if (!section) {
-      return <div>Loading...</div>;
-    }
-  
+  if (!section || !slidercontroller) {
+    return <div>Loading...</div>;
+  }
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === slidercontroller.length - 1 ? 0 : prevSlide + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? slidercontroller.length - 1 : prevSlide - 1
+    );
+  };
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    beforeChange: (_, nextSlide) => setCurrentSlide(nextSlide),
+  };
 
   return (
     <>
-    <div className="nav-bar">
-    <NavBar />
-    </div>
-    <div className="slider-conatiner">
-    <div className="slider">
-      <div className="slider-content">
-        <img src={slides[currentSlide].image} alt={slides[currentSlide].caption} />
-        <div className="slider-icons">
-          {slides.map((slide, index) => (
-            <div
-              key={index}
-              className={`slider-icon ${index === currentSlide ? "active" : ""}`}
-              onClick={() => setCurrentSlide(index)}
-            ></div>
+      <div className="nav-bar">
+        <NavBar />
+      </div>
+
+      <div className="slider-container">
+        <Slider {...settings} initialSlide={currentSlide}>
+          {slidercontroller.map((data, i) => (
+            <div key={i}>
+              <img
+                src={data.Slider_image}
+                alt="Slider Image"
+                className="slider-image"
+              />
+            </div>
           ))}
-        </div>
-      </div>
-      <div className="sliderp">
-        <img src="https://www.iconpacks.net/icons/2/free-arrow-left-icon-3099-thumb.png" alt="Left Arrow" onClick={() => setCurrentSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1)} />
-      </div>
-      <div className="slidern">
-        <img src="https://cdn.icon-icons.com/icons2/1904/PNG/512/rightarrow_121279.png" alt="Right Arrow" onClick={() => setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1)} />
-      </div> 
-     </div> 
-     <div className="text"> 
-     {section.map(item => (
-                <div key={item.Text_section_id } className="text-1">
-                <div className="elements">{item.Text_section_title}</div>
-                <div className="atributes">{item.Text_section_description}</div>
-                <div className="line"></div>     
-                    
-</div>
- ))} 
-</div> 
-<Link to={'/OurPartners'} className='btn'>
-                    <button className='btn'>OurPartners</button>
-                  </Link>
-</div>
+        </Slider>
 
- <Footer />
- </>
+        <button className="btn-prev" onClick={prevSlide}></button>
+        <button className="btn-next" onClick={nextSlide}></button>
+      </div>
+
+      <div className="text">
+        {section.map((item) => (
+          <div key={item.Text_section_id} className="text-1">
+            <div className="elements">{item.Text_section_title}</div>
+            <div className="atributes">{item.Text_section_description}</div>
+            <div className="line"></div>
+          </div>
+        ))}
+      </div>
+
+      <Link to={"/OurPartners"} className="btn">
+        <button className="btn">Our Partners</button>
+      </Link>
+
+      <Footer />
+    </>
   );
-};
+}
 
-
-export default AboutPage; 
+export default AboutPage;
